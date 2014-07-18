ed31cd694e13545f71341ad12c7ba5ea9d305032
Gary Struthers <agilej2ee@earthlink.net>
2014-07-17 18:47:24 -0700

    use file for test data

--- src/test/scala/org/gs/digraph/DigraphSuite.scala
+++ src/test/scala/org/gs/digraph/DigraphSuite.scala
@@ -6,24 +6,34 @@ import org.scalatest.FlatSpec
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.gs.digraph.fixtures.DigraphBuilder
+import org.gs.digraph.fixtures.UnweightedDigraphBuilder
 /** @author Gary Struthers
   *
   */
 @RunWith(classOf[JUnitRunner])
 class DigraphSuite extends FlatSpec {
+  val builder = new UnweightedDigraphBuilder("http://algs4.cs.princeton.edu/42directed/tinyDG.txt")
+  val tinyDG = builder.g
+  val dAGbuilder = new UnweightedDigraphBuilder("http://algs4.cs.princeton.edu/44sp/tinyDAG.txt")
+  val tinyDAG = dAGbuilder.g
+  val equals = (_: Int) == (_: Int)
   
   behavior of "a Digraph"
-  
-  it should "find vertex adjacencies" in new DigraphBuilder {
-    assert(tinyDAG.adj.flatten.corresponds(List(5,1,6,0,3,5,4,9,4,6,7,11,10,12,12))(equals))
-    assert(tinyDG.adj.flatten.corresponds(List(5,1,0,3,5,2,3,2,4,9,4,8,0,6,9,6,11,10,12,4,12,9))(equals))
+
+  it should "find vertex adjacencies" in {
+    assert(tinyDAG.adj.flatten.corresponds(
+        List(5, 1, 6, 0, 3, 5, 4, 9, 4, 6, 7, 11, 10, 12, 12))(equals))
+    assert(tinyDG.adj.flatten.corresponds(
+        List(5, 1, 0, 3, 5, 2, 3, 2, 4, 9, 4, 8, 0, 6, 9, 6, 11, 10, 12, 4, 12, 9))(equals))
   }
-  
-  it should "reverse and find vertex adjacencies" in new DigraphBuilder {
+
+  it should "reverse and find vertex adjacencies" in {
     val reversedDAG = tinyDAG.reverse
-    assert(reversedDAG.adj.flatten.corresponds(List(2,0,2,6,5,3,0,7,0,8,6,9,9,11,9))(equals))
+    assert(reversedDAG.adj.flatten.corresponds(
+        List(2, 0, 2, 6, 5, 3, 0, 7, 0, 8, 6, 9, 9, 11, 9))(equals))
 
     val reversedDG = tinyDG.reverse
-    assert(reversedDG.adj.flatten.corresponds(List(6,2,0,4,3,4,2,11,6,5,3,0,8,7,6,12,7,6,9,9,11,10))(equals))
+    assert(reversedDG.adj.flatten.corresponds(
+        List(6, 2, 0, 4, 3, 4, 2, 11, 6, 5, 3, 0, 8, 7, 6, 12, 7, 6, 9, 9, 11, 10))(equals))
   }
 } 
