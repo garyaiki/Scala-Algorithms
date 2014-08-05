4f2a46117dbdf5ba470b2d9352636edea75ff2b3
Gary Struthers <agilej2ee@earthlink.net>
2014-08-04 18:24:46 -0700

    delete worksheets

--- .worksheet/src/org.gs.map.DebugBasicSymbolTable.scala
+++
@@ -1,5 +0,0 @@
-package org.gs.symboltable
-
-object DebugBasicSymbolTable {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(126); 
-  def main(args: Array[String]) {println("Welcome to the Scala worksheet")};System.out.println("""main: (args: Array[String])Unit""")}
-}
